d6d5f1bf1160a7745a8cf46b5ad095f801d98ce2
Fabian Vilers <fabian.vilers@dev-one.com>
2014-08-01 18:51:43 +0200

    Updated API to new URl (http://my-coach.azurewebsites.net/api/expertise-domains)

---
+++ MyCoach/MyCoach.Database/dbo/Tables/ApplicationUsers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ApplicationUsers] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]     NVARCHAR (MAX) NULL,
+    [Password]     NVARCHAR (MAX) NULL,
+    [PasswordHash] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.ApplicationUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
