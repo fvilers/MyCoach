8f3f28ff9f249cc981e0418a2c5af3c1ecac8856
Sylvain Guérin <sguerin@guess-engineering.com>
2014-08-01 17:17:46 +0200

    ApplicationUserStore added

--- MyCoach/MyCoach.Data.EntityFramework/ApplicationUserStore.cs
+++ MyCoach/MyCoach.Data.EntityFramework/ApplicationUserStore.cs
@@ -1,121 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using MyCoach.Business.Domain.Model;
 
 namespace MyCoach.Data.EntityFramework
 {
-    //public class ApplicationUserStore : IUserPasswordStore<ApplicationUser, int>
-    //{
-    //    private readonly MyDoctorContext _context = new MyDoctorContext();
-    //    private bool _disposed;
+    public class ApplicationUserStore : IUserPasswordStore<ApplicationUser, int>
+    {
+        private readonly MyCoachContext _context = new MyCoachContext();
+        private bool _disposed;
 
-    //    ~DoctorUserStore()
-    //    {
-    //        Dispose(false);
-    //    }
+        ~ApplicationUserStore()
+        {
+            Dispose(false);
+        }
 
-    //    public void Dispose()
-    //    {
-    //        Dispose(true);
-    //        GC.SuppressFinalize(this);
-    //    }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-    //    public async Task CreateAsync(Doctor user)
-    //    {
-    //        ThrowIfDisposed();
-    //        if (user == null) throw new ArgumentNullException("user");
+        public async Task CreateAsync(ApplicationUser user)
+        {
+            ThrowIfDisposed();
+            if (user == null) throw new ArgumentNullException("user");
 
-    //        _context.Doctors.Add(user);
-    //        await _context.SaveChangesAsync();
-    //    }
+            _context.ApplicationUsers.Add(user);
+            await _context.SaveChangesAsync();
+        }
 
-    //    public async Task UpdateAsync(Doctor user)
-    //    {
-    //        ThrowIfDisposed();
-    //        if (user == null) throw new ArgumentNullException("user");
+        public async Task UpdateAsync(ApplicationUser user)
+        {
+            ThrowIfDisposed();
+            if (user == null) throw new ArgumentNullException("user");
 
-    //        _context.Entry(user).State = EntityState.Modified;
-    //        await _context.SaveChangesAsync();
-    //    }
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
 
-    //    public async Task DeleteAsync(Doctor user)
-    //    {
-    //        ThrowIfDisposed();
-    //        if (user == null) throw new ArgumentNullException("user");
+        public async Task DeleteAsync(ApplicationUser user)
+        {
+            ThrowIfDisposed();
+            if (user == null) throw new ArgumentNullException("user");
 
-    //        _context.Doctors.Remove(user);
-    //        await _context.SaveChangesAsync();
-    //    }
+            _context.ApplicationUsers.Remove(user);
+            await _context.SaveChangesAsync();
+        }
 
-    //    public async Task<Doctor> FindByIdAsync(int userId)
-    //    {
-    //        ThrowIfDisposed();
+        public async Task<ApplicationUser> FindByIdAsync(int userId)
+        {
+            ThrowIfDisposed();
 
-    //        return await FindAsync(doctor => doctor.Id == userId);
-    //    }
+            return await FindAsync(user => user.Id == userId);
+        }
 
-    //    public async Task<Doctor> FindByNameAsync(string userName)
-    //    {
-    //        ThrowIfDisposed();
-    //        if (userName == null) throw new ArgumentNullException("userName");
+        public async Task<ApplicationUser> FindByNameAsync(string userName)
+        {
+            ThrowIfDisposed();
+            if (userName == null) throw new ArgumentNullException("userName");
 
-    //        return await FindAsync(doctor => doctor.UserName.ToUpper() == userName.ToUpper());
-    //    }
+            return await FindAsync(user => user.UserName.ToUpper() == userName.ToUpper());
+        }
 
-    //    public Task SetPasswordHashAsync(Doctor user, string passwordHash)
-    //    {
-    //        ThrowIfDisposed();
-    //        if (user == null) throw new ArgumentNullException("user");
+        public Task SetPasswordHashAsync(ApplicationUser user, string passwordHash)
+        {
+            ThrowIfDisposed();
+            if (user == null) throw new ArgumentNullException("user");
 
-    //        user.Password.Hash = passwordHash;
-    //        return Task.FromResult(0);
-    //    }
+            user.PasswordHash = passwordHash;
+            return Task.FromResult(0);
+        }
 
-    //    public Task<string> GetPasswordHashAsync(Doctor user)
-    //    {
-    //        ThrowIfDisposed();
-    //        if (user == null) throw new ArgumentNullException("user");
+        public Task<string> GetPasswordHashAsync(ApplicationUser user)
+        {
+            ThrowIfDisposed();
+            if (user == null) throw new ArgumentNullException("user");
 
-    //        return Task.FromResult(user.Password.Hash);
-    //    }
+            return Task.FromResult(user.PasswordHash);
+        }
 
-    //    public Task<bool> HasPasswordAsync(Doctor user)
-    //    {
-    //        ThrowIfDisposed();
-    //        if (user == null) throw new ArgumentNullException("user");
+        public Task<bool> HasPasswordAsync(ApplicationUser user)
+        {
+            ThrowIfDisposed();
+            if (user == null) throw new ArgumentNullException("user");
 
-    //        return Task.FromResult(!String.IsNullOrWhiteSpace(user.Password.Hash));
-    //    }
+            return Task.FromResult(!String.IsNullOrWhiteSpace(user.PasswordHash));
+        }
 
-    //    protected virtual void Dispose(bool disposing)
-    //    {
-    //        if (_disposed)
-    //        {
-    //            return;
-    //        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
 
-    //        if (disposing)
-    //        {
-    //            _context.Dispose();
-    //        }
+            if (disposing)
+            {
+                _context.Dispose();
+            }
 
-    //        _disposed = true;
-    //    }
+            _disposed = true;
+        }
 
-    //    private void ThrowIfDisposed()
-    //    {
-    //        if (_disposed)
-    //        {
-    //            throw new ObjectDisposedException(GetType().Name);
-    //        }
-    //    }
+        private void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
 
-    //    private Task<Doctor> FindAsync(Expression<Func<Doctor, bool>> predicate)
-    //    {
-    //        return _context.Doctors.FirstOrDefaultAsync(predicate);
-    //    }
-    //}
+        private Task<ApplicationUser> FindAsync(Expression<Func<ApplicationUser, bool>> predicate)
+        {
+            return _context.ApplicationUsers.FirstOrDefaultAsync(predicate);
+        }
+    }
 }
