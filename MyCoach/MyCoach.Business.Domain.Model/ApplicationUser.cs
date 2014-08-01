430621e3ddaf0f5330b426fd473c6496f476a9ae
Sylvain Guérin <sguerin@guess-engineering.com>
2014-08-01 16:29:56 +0200

    Data and Data.EntityFramework added

---
+++ MyCoach/MyCoach.Business.Domain.Model/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace MyCoach.Business.Domain.Model
+{
+    public class ApplicationUser : IUser<int>
+    {
+
+        public int Id {get; set; }
+
+        public string UserName { get; set; }
+    }
+}
