a9c3add1f937a12bcd130e444788c6b3ae58d0ce
Fabian Vilers <fabian.vilers@dev-one.com>
2014-08-01 14:47:35 +0200

    Updated home page with search form

--- MyCoach/MyCoach.Web.MainSite/Controllers/HomeController.cs
+++ MyCoach/MyCoach.Web.MainSite/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace MyCoach.Web.MainSite.Controllers
 {
+    [RoutePrefix("")]
     public class HomeController : Controller
     {
+        [Route("")]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [Route("search")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Search()
+        {
+            throw new NotImplementedException();
         }
     }
 }
