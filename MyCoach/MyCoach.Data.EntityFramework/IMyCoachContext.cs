00f3d75184389eefdfb98f12258b2904883bf667
Sylvain Guérin <sguerin@guess-engineering.com>
2014-08-01 16:55:05 +0200

    MyCoachContext added (and some interfaces...)

---
+++ MyCoach/MyCoach.Data.EntityFramework/IMyCoachContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using MyCoach.Business.Domain.Model;
+
+namespace MyCoach.Data.EntityFramework
+{
+    public interface IMyCoachContext : IUnitOfWork
+    {
+        IDbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ MyCoach/MyCoach.Data/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MyCoach.Data
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChanges();
+    }
+}
