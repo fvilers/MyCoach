be83df053700c56c75df35197351ddc44f03e34c
Fabian Vilers <fabian.vilers@dev-one.com>
2014-08-01 22:47:40 +0200

    Updated database project

--- MyCoach/MyCoach.Database/dbo/Tables/ExpertiseDomains.sql
+++ MyCoach/MyCoach.Database/dbo/Tables/ExpertiseDomains.sql
@@ -1,14 +1,13 @@
 ﻿CREATE TABLE [dbo].[ExpertiseDomains] (
-    [Id]              INT            IDENTITY (1, 1) NOT NULL,
-    [Name]            NVARCHAR (MAX) NULL,
-    [Slug]            NVARCHAR (MAX) NULL,
-    [CoachProfile_Id] INT            NULL,
-    CONSTRAINT [PK_dbo.ExpertiseDomains] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_dbo.ExpertiseDomains_dbo.CoachProfiles_CoachProfile_Id] FOREIGN KEY ([CoachProfile_Id]) REFERENCES [dbo].[CoachProfiles] ([Id])
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NULL,
+    [Slug] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.ExpertiseDomains] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
 
+
+
 GO
-CREATE NONCLUSTERED INDEX [IX_CoachProfile_Id]
-    ON [dbo].[ExpertiseDomains]([CoachProfile_Id] ASC);
+
 
