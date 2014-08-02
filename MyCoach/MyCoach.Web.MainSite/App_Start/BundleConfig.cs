296bef1d602fe22acbff2737896a131c9c5f6e1b
Fabian Vilers <fabian.vilers@dev-one.com>
2014-08-02 19:00:14 +0200

    Updated layout with Dave's work

--- MyCoach/MyCoach.Web.MainSite/App_Start/BundleConfig.cs
+++ MyCoach/MyCoach.Web.MainSite/App_Start/BundleConfig.cs
@@ -6,25 +6,25 @@ namespace MyCoach.Web.MainSite
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery")
-                .Include("~/Scripts/jquery-{version}.js")
-                .Include("~/Scripts/site.js")
-            );
+            //bundles.Add(new ScriptBundle("~/bundles/jquery")
+            //    .Include("~/Scripts/jquery-{version}.js")
+            //    .Include("~/Scripts/site.js")
+            //);
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
-                .Include("~/Scripts/jquery.validate.js")
-                .Include("~/Scripts/jquery.validate.unobtrusive.js")
-            );
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+            //    .Include("~/Scripts/jquery.validate.js")
+            //    .Include("~/Scripts/jquery.validate.unobtrusive.js")
+            //);
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
-                .Include("~/Scripts/bootstrap.js")
-                .Include("~/Scripts/respond.js")
-            );
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+            //    .Include("~/Scripts/bootstrap.js")
+            //    .Include("~/Scripts/respond.js")
+            //);
 
-            bundles.Add(new StyleBundle("~/Content/css")
-                .Include("~/Content/bootstrap.css")
-                .Include("~/Content/site.css")
-            );
+            //bundles.Add(new StyleBundle("~/Content/css")
+            //    .Include("~/Content/bootstrap.css")
+            //    .Include("~/Content/site.css")
+            //);
         }
     }
 }
